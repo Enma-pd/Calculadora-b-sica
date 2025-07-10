6d09932a7b34a75b030943c98a6cac264954c16c
sin nombre <no@nomail.es>
2025-07-10 12:17:47 +0200

    a resolver un poco todavia (coche)

---
+++ src/main/java/es/cic/curso25/Coche.java
@@ -0,0 +1,26 @@
+package es.cic.curso25;
+
+public class Coche {
+
+    private int velocidad = 0;
+    public static final double CONSUMO_INSTANTANEO = 4.3;
+
+    public int getVelocidad(){
+        return velocidad;
+    }
+     public double getConsumo(){
+
+        return ((double)velocidad) /100 *CONSUMO_INSTANTANEO;
+    }
+     public int acelerar(int incrementoVelocidad){
+        this.velocidad += incrementoVelocidad;
+        return this.velocidad;
+        
+    }
+     public int frenar(int decrementoVelocidad){
+        this.velocidad -= decrementoVelocidad;
+        return this.velocidad;
+        
+    }
+
+}
